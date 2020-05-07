efa58d2adf0ef522463084d1e187584040256981
hasan habib <iamhasanhabib@yahoo.com>
2020-05-07 12:36:39 +0600

    Mycogen

--- contents/js/script.js
+++ contents/js/script.js
@@ -1,17 +1,75 @@
-var openmodalbtn = document.querySelector('.open-modal-btn');
-var closemodalbtn = document.querySelector('.modal-icon');
+var video = document.querySelector(".bg-vid");
+var btn = document.querySelector(".vid-btn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+
+
+
+
+var openRecModalBtn = document.querySelector('.open-rec-modal-btn');
+var openExpModalBtn = document.querySelector('.open-exp-modal-btn');
+var openEduModalBtn = document.querySelector('.open-edu-modal-btn');
+
+var closeRecModalBtn = document.querySelector('.close-rec-modal-btn');
+var closeExpModalBtn = document.querySelector('.close-exp-modal-btn');
+var closeEduModalBtn = document.querySelector('.close-edu-modal-btn');
+
 var recModal = document.querySelector('.recommendation-modal');
+var expModal = document.querySelector('.experience-modal');
+var eduModal = document.querySelector('.education-modal');
+
 var overlay = document.querySelector('#overlay');
 
+var navLink = document.querySelector('.nav-link');
 
 
 
-openmodalbtn.addEventListener('click', function() {
+
+
+overlay.addEventListener('click', function() {
+        recModal.classList.remove('active');
+        expModal.classList.remove('active');
+        eduModal.classList.remove('active');
+        overlay.classList.remove('active');
+});
+
+
+openRecModalBtn.addEventListener('click', function() {
         recModal.classList.add('active');
         overlay.classList.add('active');
 });
 
-closemodalbtn.addEventListener('click', function() {
+closeRecModalBtn.addEventListener('click', function() {
         recModal.classList.remove('active');
+        overlay.classList.remove('active');
+});
+
+
+openExpModalBtn.addEventListener('click', function() {
+        expModal.classList.add('active');
+        overlay.classList.add('active');
+});
+
+closeExpModalBtn.addEventListener('click', function() {
+        expModal.classList.remove('active');
+        overlay.classList.remove('active');
+});
+
+
+openEduModalBtn.addEventListener('click', function() {
+        eduModal.classList.add('active');
+        overlay.classList.add('active');
+});
+
+closeEduModalBtn.addEventListener('click', function() {
+        eduModal.classList.remove('active');
         overlay.classList.remove('active');
 });
